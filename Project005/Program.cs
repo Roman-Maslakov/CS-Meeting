251c752ebd87739cec667c5fe201c1e8ae3c8503
Ramses <rexid44@gmail.com>
2023-02-11 23:40:25 +1000

    Greetings

---
+++ Project005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+    Console.WriteLine("АРИКУ! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
